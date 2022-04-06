74cd5c39588baba61a52230a2c4c4efbaeb7f335
Automation FC <khanhlinh.do.93@gmail.com>
2022-04-06 18:12:46 +0700

    Update_Homework_guru99

--- test selenium/webdriver/Homework_Login.java
+++ test selenium/webdriver/Homework_Login.java
@@ -24,31 +24,65 @@ public class Homework_Login {
 		driver.get("http://live.techpanda.org/");
 	}
 
-	@Test
-	public void Step_01_Click_My_Account() {
+
+	public void TC_01_Login_empty_account() {
 		
 		driver.findElement(By.xpath("//a[@class ='skip-link skip-account']//span[@class='label']")).click();
-		
+		driver.findElement(By.xpath("//a[@title='Log In']")).click();
+		driver.findElement(By.xpath("//button[@id='send2']")).click();
+		Assert.assertTrue(driver.findElement(By.xpath("//div[@id='advice-required-entry-email']")).isDisplayed());
 	}
 
-	@Test
-	public void Step_02_Click_Log_In() {
-		
+	
+	
+	
+	public void TC_02_Login_Invalid_Email() {
+		driver.findElement(By.xpath("//a[@class ='skip-link skip-account']//span[@class='label']")).click();
 		driver.findElement(By.xpath("//a[@title='Log In']")).click();
-		
+		driver.findElement(By.xpath("//input[@id='email']")).sendKeys("123456@123456.123455");
+		driver.findElement(By.xpath("//input[@type='password']")).sendKeys("123456");
+		driver.findElement(By.xpath("//button[@id='send2']")).click();
+		Assert.assertTrue(driver.findElement(By.xpath("//div[@id='advice-validate-email-email']")).isDisplayed());
+	}
+	
+
+	public void TC_03_Login_Invalid_PW() {
+		driver.findElement(By.xpath("//a[@class ='skip-link skip-account']//span[@class='label']")).click();
+		driver.findElement(By.xpath("//a[@title='Log In']")).click();
+		driver.findElement(By.xpath("//input[@id='email']")).sendKeys("khanhlinh@gmail.com");
+		driver.findElement(By.xpath("//input[@type='password']")).sendKeys("123");
+		driver.findElement(By.xpath("//button[@id='send2']")).click();
+		Assert.assertTrue(driver.findElement(By.xpath("//div[@id='advice-validate-password-pass']")).isDisplayed());
+	}
+	
+	
+	public void TC_04_Login_Incorrect_emailpw() {
+		driver.findElement(By.xpath("//a[@class ='skip-link skip-account']//span[@class='label']")).click();
+		driver.findElement(By.xpath("//a[@title='Log In']")).click();
+		driver.findElement(By.xpath("//input[@id='email']")).sendKeys("khanhlinh@gmail.com");
+		driver.findElement(By.xpath("//input[@type='password']")).sendKeys("123456");
+		driver.findElement(By.xpath("//button[@id='send2']")).click();
+		Assert.assertTrue(driver.findElement(By.xpath("//li[@class='error-msg']//span")).isDisplayed());
 	}
 	
 	@Test
-	public void Step_03_Click_Send2() {
-		
-		driver.findElement(By.xpath("//button[@id='send2']")).click();
-		Assert.assertTrue(driver.findElement(By.xpath("//div[@id='advice-required-entry-email']")).isDisplayed());
-		
+	public void TC_05_Create_Account() {
+		driver.findElement(By.xpath("//a[@class ='skip-link skip-account']//span[@class='label']")).click();
+		driver.findElement(By.xpath("//a[@title='My Account']")).click();
+		driver.findElement(By.xpath("//a[@title='Create an Account']")).click();
+		driver.findElement(By.xpath("//input[@id='firstname']")).sendKeys("Linh");
+		driver.findElement(By.xpath("//input[@id='middlename']")).sendKeys("Tran");
+		driver.findElement(By.xpath("//input[@id='lastname']")).sendKeys("Do");
+		driver.findElement(By.xpath("//input[@id='email_address']")).sendKeys("khanhlinh@gmail.com");
+		driver.findElement(By.xpath("//input[@id='password']")).sendKeys("123456");
+		driver.findElement(By.xpath("//input[@id='confirmation']")).sendKeys("123456");
+		driver.findElement(By.xpath("//button[@title='Register']")).click();
+		Assert.assertTrue(driver.findElement(By.xpath("//li[@class='success-msg']//span")).isDisplayed());
 	}
 	
 	@AfterTest
 	public void afterTest() {
-		driver.quit();
+		//driver.quit();
 	}
 
 }
